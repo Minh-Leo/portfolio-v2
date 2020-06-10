ed41a88dc2ac3c97c1650a9ffd05c6bc5b94fed6
Minh Bui <tranduyminh.bui@gmail.com>
2020-06-10 21:36:42 +1000

    first deployment

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -1,122 +1,110 @@
-import * as THREE from 'three'
-import Tile from './Tile'
-import DetailView from './Detail'
-import { ev } from './utils/utils'
+import * as THREE from "three";
+import Tile from "./Tile";
+import DetailView from "./Detail";
+import { ev } from "./utils/utils";
 
-import trippyShader from '../glsl/trippyShader.glsl'
-import shapeShader from '../glsl/shapeShader.glsl'
-import revealShader from '../glsl/revealShader.glsl'
-import gooeyShader from '../glsl/gooeyShader.glsl'
-import waveShader from '../glsl/waveShader.glsl'
+import revealShader from "../glsl/revealShader.glsl";
 
-const perspective = 800
+const perspective = 800;
 
-const shaders = [
-    trippyShader,
-    shapeShader,
-    gooeyShader,
-    waveShader,
-    revealShader,
-]
+const shaders = [revealShader];
 
-const durations = [
-    0.5,
-    0.5,
-    0.5,
-    0.8,
-    0.8,
-]
+const durations = [0.8];
 
 export default class Scene {
-
     constructor($scene) {
-        this.container = $scene
-        this.$tiles = document.querySelectorAll('.slideshow-list__el')
+        this.container = $scene;
+        this.$tiles = document.querySelectorAll(".slideshow-list__el");
 
-        this.W = window.innerWidth
-        this.H = window.innerHeight
+        this.W = window.innerWidth;
+        this.H = window.innerHeight;
 
-        this.mouse = new THREE.Vector2(0, 0)
-        this.activeTile = null
+        this.mouse = new THREE.Vector2(0, 0);
+        this.activeTile = null;
 
-        this.start()
+        this.start();
 
-        this.detailview = new DetailView()
+        this.detailview = new DetailView();
 
-
-        this.bindEvent()
+        this.bindEvent();
     }
 
     bindEvent() {
-        document.addEventListener('toggleDetail', ({ detail: shouldOpen }) => { this.onToggleView(shouldOpen) })
+        document.addEventListener("toggleDetail", ({ detail: shouldOpen }) => {
+            this.onToggleView(shouldOpen);
+        });
 
-        window.addEventListener('resize', () => { this.onResize() })
+        window.addEventListener("resize", () => {
+            this.onResize();
+        });
     }
 
-
     start() {
-        this.mainScene = new THREE.Scene()
-        this.initCamera()
-        this.initLights()
+        this.mainScene = new THREE.Scene();
+        this.initCamera();
+        this.initLights();
 
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.container,
             alpha: true,
-        })
-        this.renderer.setSize(this.W, this.H)
-        this.renderer.setPixelRatio(window.devicePixelRatio)
+        });
+        this.renderer.setSize(this.W, this.H);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
 
-        this.tiles = Array.from(this.$tiles).map(($el, i) => new Tile($el, this, durations[i], shaders[i]))
+        this.tiles = Array.from(this.$tiles).map(
+            ($el, i) => new Tile($el, this, durations[0], shaders[0])
+        );
 
-        this.update()
+        this.update();
     }
 
     initCamera() {
-        const fov = (180 * (2 * Math.atan(this.H / 2 / perspective))) / Math.PI
+        const fov = (180 * (2 * Math.atan(this.H / 2 / perspective))) / Math.PI;
 
-        this.camera = new THREE.PerspectiveCamera(fov, this.W / this.H, 1, 10000)
-        this.camera.position.set(0, 0, perspective)
+        this.camera = new THREE.PerspectiveCamera(
+            fov,
+            this.W / this.H,
+            1,
+            10000
+        );
+        this.camera.position.set(0, 0, perspective);
     }
 
     initLights() {
-        const ambientlight = new THREE.AmbientLight(0xffffff, 2)
-        this.mainScene.add(ambientlight)
+        const ambientlight = new THREE.AmbientLight(0xffffff, 2);
+        this.mainScene.add(ambientlight);
     }
-
-
-
 
     /* Handlers
     --------------------------------------------------------- */
 
     onResize() {
-        this.W = window.innerWidth
-        this.H = window.innerHeight
+        this.W = window.innerWidth;
+        this.H = window.innerHeight;
 
-        this.camera.aspect = this.W / this.H
+        this.camera.aspect = this.W / this.H;
 
-        this.camera.updateProjectionMatrix()
-        this.renderer.setSize(this.W, this.H)
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.W, this.H);
     }
 
     onToggleView({ target, open }) {
-        this.activeTile = target // !== undefined ? target : this.activeTile
+        this.activeTile = target; // !== undefined ? target : this.activeTile
 
-        ev('lockScroll', { lock: open })
-        ev('tile:zoom', { tile: this.activeTile, open })
+        ev("lockScroll", { lock: open });
+        ev("tile:zoom", { tile: this.activeTile, open });
     }
 
     /* Actions
     --------------------------------------------------------- */
 
     update() {
-        requestAnimationFrame(this.update.bind(this))
+        requestAnimationFrame(this.update.bind(this));
 
         this.tiles.forEach((tile) => {
-            tile.update()
-        })
+            tile.update();
+        });
 
-        this.renderer.render(this.mainScene, this.camera)
+        this.renderer.render(this.mainScene, this.camera);
     }
-
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,23 @@
-import '../sass/styles.scss'
-import Stage from './Stage'
-import Layout from './Layout'
+import "../sass/styles.scss";
+import Stage from "./Stage";
+import Layout from "./Layout";
 
-const APP = window.APP || {}
+const APP = window.APP || {};
 
-/*-----------------------------------------------------------------------------------*/
-/*  01. INIT
-/*-----------------------------------------------------------------------------------*/
+/*  01. INIT */
 
 const initApp = () => {
-    window.APP = APP
+    window.APP = APP;
 
-    APP.Stage = new Stage()
-    APP.Layout = new Layout()
-}
+    APP.Stage = new Stage();
+    APP.Layout = new Layout();
+};
 
-if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
-    initApp()
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    initApp();
 } else {
-    document.addEventListener('DOMContentLoaded', initApp)
+    document.addEventListener("DOMContentLoaded", initApp);
 }
